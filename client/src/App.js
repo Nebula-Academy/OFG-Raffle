506ee1c04da8979d3e2cc21ac5e4619aed6762e2
bayarens <bay.arens@gmail.com>
2021-06-22 14:00:52 -0400

    detailed view fixed up

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import LandingPage from './components/LandingPage';
 function App() {
   return (
       <div>
-         <LandingPage/>
+         <DetailedView/>
       </div>
   );
 }
--- client/src/components/DetailedView.js
+++ client/src/components/DetailedView.js
@@ -3,6 +3,9 @@ import './DetailedView.css'
 
 class DetailedView extends React.Component {
 
+
+
+
     changeTicketBar() {
         // ticketCounter.innertext = this.ticketsSold;
         // let width = this.ticketsSold / 
@@ -10,21 +13,23 @@ class DetailedView extends React.Component {
 
     render() {
         return (
-            <div className='mainContianer'>
-                <h3 className='itemTitle'>
-                    {/* {this.props.itemName}  */}
-                    Place Holder
-                </h3>
-                <img className='raffleItem' src='https://static.frame.work/pttnfnm54ipt03wzyqhzlwfk4f6t'>
-                    {/* {this.props.img} should go in atributes? */}
-                </img>
-                <p className='itemInfo'>
-                    This item is a place holder, It was provided by no one and has a market value of absolutely nothing. All proceedes from purchased tickets for this item will go directly into my back pocket
-                    {/* {this.props.info} */}
-                </p>
-                <div className='ticketCounter'> 100/83 </div>
-                <div className='buttonWrapper'>
-                    <button className='purchaseButton'>Buy Ticket</button>
+            <div className='mainContianerWrapper'>
+                <div className='mainContianer'>
+                    <h3 className='itemTitle'>
+                        {/* {this.props.itemName}  */}
+                        Laptop
+                    </h3>
+                    <img className='raffleItem' src='https://static.frame.work/pttnfnm54ipt03wzyqhzlwfk4f6t'>
+                        {/* {this.props.img} should go in atributes? */}
+                    </img>
+                    <p className='itemInfo'>
+                        This item is a place holder, It was provided by no one and has a market value of absolutely nothing. All proceedes from purchased tickets for this item will go directly into my back pocket
+                        {/* {this.props.info} */}
+                    </p>
+                    <div className='ticketCounter'> 100/83 </div>
+                    <div className='buttonWrapper'>
+                        <button className='purchaseButton'>Buy Ticket</button>
+                    </div>
                 </div>
             </div>
         )
