f0aa1094bd557ad61db13373535d7476bde51ff3
bayarens <bay.arens@gmail.com>
2021-06-23 14:12:57 -0400

    almost done with grid view

--- client/src/components/GridView.js
+++ client/src/components/GridView.js
@@ -6,15 +6,27 @@ class GridView extends React.Component{
         raffleItems: [
             {
                 title: 'Laptop',
-                img: 'placeholder',
+                img: 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE3oYjc?ver=e1aa&q=90&m=6&h=200&w=200&b=%23FFFFFFFF&o=f&aim=true',
                 ticketsSold: '100/83',
                 ticketPrice: '$5'
             },
             {
                 title: 'Laptop',
-                img: 'placeholder',
+                img: 'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE3oYjc?ver=e1aa&q=90&m=6&h=200&w=200&b=%23FFFFFFFF&o=f&aim=true',
                 ticketsSold: '100/83',
                 ticketPrice: '$5'
+            },
+            {
+                title: 'TV',
+                img: 'placeholder',
+                ticketsSold: '100/65',
+                ticketPrice: '$10'
+            },
+            {
+                title: 'Car',
+                img: 'placeholder',
+                ticketsSold: '100/47',
+                ticketPrice: '$100'
             }
         ]
     }
@@ -26,9 +38,14 @@ class GridView extends React.Component{
                 {this.state.raffleItems.map(raffleItem => <div className='itemContainer' key={raffleItem.title}>  
                 <h3 className='ItemName'> {raffleItem.title}</h3>
                 <img className='ItemImg' src={raffleItem.img}></img>
+                <div className='TicketHolder'>
                 <p className='Tickets'> {raffleItem.ticketsSold}</p>
-                <p className='TicketPrice'>{raffleItem.ticketPrice}</p>
-                <button className='GoTo'>View</button>
+                
+                <p className='Tickets'>{raffleItem.ticketPrice}</p>
+                </div>
+                <div className='GoTo'>
+                <button>View</button>
+                </div>
                 </div>)}
             </div>
         )
