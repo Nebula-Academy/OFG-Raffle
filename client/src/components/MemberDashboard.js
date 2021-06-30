a6f44c48f4f350857c09a2b111f60bdbd14e1aac
luciannebula <lucianbryan008215@gmail.com>
2021-06-30 15:10:17 -0400

    updatemember

--- client/src/components/MemberDashboard.js
+++ client/src/components/MemberDashboard.js
@@ -4,45 +4,64 @@ import React from 'react';
 class MemberDashboard extends React.Component {
     render() {
         return (
-            <div>
+            <div className='member-dashboard-container'>
                 <h1>
-                    <a>
-                        Member Profile Page
-                    </a>
-                </h1>
-                <nav>
-                    <div className='memberdashboard'>
-                        <form>
-                            <label>First_Name
-                                <input className='membutton' onClick={this.firstName} />
-                            </label>
-                            <label>Last_Name
-                                <input className='membutton' onclick={this.lastName} />
-                            </label>
-                            <label>Phone
-                                <input className='membutton' onClick={this.phone} />
-                            </label>
-                            <label>Email
-                                <input className='membutton' onClick={this.email} />
-                            </label>
-                            <label>Address1
-                                <input className='membutton' onClick={this.address1} />
-                            </label>
-                            <label>Address2
-                                <input className='membutton' onClick={this.address2} />
-                            </label>
-                            <label>City
-                                <input className='membutton' onClick={this.city} />
-                            </label>
-                            <label>State
-                                <input className='membutton' onClick={this.state} />
-                            </label>
 
-                        </form>
+                    Member Profile Page
+
+                </h1>
+                <div className='member-dashboard'>
+                    <form>
+                        <label>First_Name
+                            <input className='membutton' onClick={this.firstName} />
+                        </label>
+                        <label>Last_Name
+                            <input className='membutton' onclick={this.lastName} />
+                        </label>
+                        <label>Phone
+                            <input className='membutton' onClick={this.phone} />
+                        </label>
+                        <label>Email
+                            <input className='membutton' onClick={this.email} />
+                        </label>
+                        <label>Address1
+                            <input className='membutton' onClick={this.address1} />
+                        </label>
+                        <label>Address2
+                            <input className='membutton' onClick={this.address2} />
+                        </label>
+                        <label>City
+                            <input className='membutton' onClick={this.city} />
+                        </label>
+                        <label>State
+                            <input className='membutton' onClick={this.state} />
+                        </label>
+
+                    </form>
+                </div>
+                <div className='raf-bio'>
+                    <h2>RAFFLE</h2>
+                    <div>
+                        <label>RAFFLE TITLE
+                            <input className='rafbutton' onclick={this.raftitle} />
+                        </label>
+                        <label>RAFFLE DESCRIPTION
+                            <input className='rafbutton' onclick={this.rafdescription} />
+                        </label>
+                        <label>TOTAL TICKETS
+                            <input className='rafbutton' onclick={this.total} />
+                        </label>
+                        <label>TICKET PRICE
+                            <input className='rafbutton' onclick={this.price} />
+                        </label>
+                        <label>TICKETS SOLD
+                            <input className='rafbutton' onclick={this.sold} />
+                        </label>
                     </div>
-                </nav>
+
+                </div>
             </div >
-            
+
         )
     }
 }
