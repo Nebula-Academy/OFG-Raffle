d9a26ed3a37dfa1cf30aa8f2278a4f52616e9321
bayarens <bay.arens@gmail.com>
2021-06-30 09:47:30 -0400

    img turned into button

--- client/src/components/GridView.js
+++ client/src/components/GridView.js
@@ -11,7 +11,7 @@ class GridView extends React.Component {
         addRaffleModal: false
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.refresh();
     }
 
@@ -24,7 +24,7 @@ class GridView extends React.Component {
         this.setState({ addRaffleModal: true })
     }
 
-    closeAddRaffleModal= () => {
+    closeAddRaffleModal = () => {
         this.setState({ addRaffleModal: false })
 
     }
@@ -40,22 +40,19 @@ class GridView extends React.Component {
                     aria-labelledby="simple-modal-title"
                     aria-describedby="simple-modal-description"
                 >
-                    <AddRaffle close={this.closeAddRaffleModal} refresh={this.refresh}/>
+                    <AddRaffle close={this.closeAddRaffleModal} refresh={this.refresh} />
                 </Modal>
                 <button className='createRaffle' onClick={this.openAddRaffleModal}> Create Raffle </button>
                 <div id='grid'>
                     {this.state.raffleItems.map(raffleItem => <div className='itemContainer' key={raffleItem.title}>
                         <h3 className='ItemName'> {raffleItem.title}</h3>
-                        <img className='ItemImg' src={raffleItem.image_file_path}></img>
+                        <Link to={`/raffle/${raffleItem.raffle_id}`}>
+                            <img className='ItemImg' src={raffleItem.image_file_path}></img>
+                        </Link>
                         <div className='TicketHolder'>
                             <p className='Tickets'> {raffleItem.tickets_sold}/{raffleItem.total_tickets}</p>
 
                             <p className='Tickets'>${raffleItem.ticket_price}</p>
-                        </div>
-                        <div className='GoTo'>
-                            <Link to={`/raffle/${raffleItem.raffle_id}`}>
-                                <button>View</button>
-                            </Link>
                         </div>
                     </div>)}
                 </div>
