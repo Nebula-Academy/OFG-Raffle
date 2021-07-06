d945f09eb5359f9aba06b6bbdb51bf2c69c4fe7f
tefedrb-enterprise <tefebell@gmail.com>
2021-07-06 09:53:58 -0400

    adding user dummy data

--- client/src/App.js
+++ client/src/App.js
@@ -7,30 +7,47 @@ import GridView from './components/GridView';
 import LandingPage from './components/LandingPage'
 import AccountVerification from './components/AccountVerification'
 import LoginPage from './components/LoginPage'
-function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Route exact path="/signup">
-        <h1>
-          This is the sign Up Page!
-        </h1>
-      </Route>
-      <Route exact path="/accountverification">
-        <AccountVerification/>
-      </Route>
-      <Route path="/memberinfo">
-        <MemberDashboard />
-      </Route>
-      <Route path="/" exact>
-        <LandingPage />
-      </Route>
-      <Route path="/raffles">
-        <GridView />
-      </Route>
-      <Route path="/raffle/:id" component={DetailedView} />
-    </BrowserRouter>
-  );
+import React from 'react';
+import { users } from './dummyUsers';
+
+class App extends React.Component {
+  state = {
+    currentUser: {}
+  }
+  
+  componentDidMount(){
+    this.setState({ currentUser: this.getCurrentUser()});
+  }
+
+  getCurrentUser(){
+    return users[1];
+  }
+
+  render(){
+    return (
+      <BrowserRouter>
+        <Header user={this.state.currentUser}/>
+        <Route exact path="/signup">
+          <h1>
+            This is the sign Up Page!
+          </h1>
+        </Route>
+        <Route exact path="/accountverification">
+          <AccountVerification user={this.state.currentUser}/>
+        </Route>
+        <Route path="/memberinfo">
+          <MemberDashboard user={this.state.currentUser}/>
+        </Route>
+        <Route path="/" exact>
+          <LandingPage user={this.state.currentUser}/>
+        </Route>
+        <Route path="/raffles">
+          <GridView user={this.state.currentUser}/>
+        </Route>
+        <Route path="/raffle/:id" component={<><DetailedView user={this.state.currentUser}/></>} />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
