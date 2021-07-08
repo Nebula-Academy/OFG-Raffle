1e33aaafaa2eb7380a6498bb20db3a7c57957554
Shaundel5597 <naunie08@yahoo.com>
2021-07-08 14:15:25 -0400

    minor changes

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,24 +1,39 @@
 import React from 'react'
 import './LandingPage.css'
+import {getTable} from './NetworkRequests'
 
 class LandingPage extends React.Component {
     state = {
         currRaffles: [],
+        pastWinners: []
     }
 
     componentDidMount() {
         // here we're fetching raffles from our database
         // then we're setting our state with those raffles
-
+        getTable("raffle")
+        .then(res => this.setState({currRaffles:res}))
+        // need to make network request that finds the past raffle winners associated with winners table
+        // getTable('winner')
+        // .then(res => this.setState({pastWinners:res}))
     }
 
     currentRaffles() {
         return this.state.currRaffles.map(raffle => (
-            <div className="button1">
+            <div className="current-raffle-card">
                 <img src={raffle.image_file_path}/>
                 <h2> {raffle.title} </h2>
                 <p> {raffle.ticket_price} </p>
                 <button>test</button>
+            </div>
+        ))
+
+    }
+
+    pastWins() {
+        return this.state.pastWinners.map(raffle => (
+            <div className="past-winners-card">
+                <img src={raffle.image_file_path}/>
             </div>
         ))
     }
@@ -31,15 +46,17 @@ class LandingPage extends React.Component {
                 <h1>Welcome To Our Future Generation </h1>
                 <h1>Raffles</h1>
                 <div className="row">
-                    <div className='column box'>
+                    <div className='column-box'>
                         <h4>Current Raffles</h4>
-                        {this.currentRaffles()}
+                        <div className="current-raffles-container"> 
+                            {this.currentRaffles()}
+                        </div>
                     </div>
-                    <div className="column box">
+                    <div className="column-box">
                         <h4>Past Winners</h4>
-                        <button>test</button>
-                        <button>test</button>
-                        <button>test</button>
+                        <div className="past-winners-container">
+                            {this.pastWins()}
+                        </div>
                     </div>
                 </div>
 
--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,82 +1,35 @@
-
+import React from 'react';
 import './LoginPage.css'
 
-import React from 'react';
-import { AmplifyAuthenticator, AmplifySignUp, AmplifySignOut } from '@aws-amplify/ui-react';
-import { Auth, Hub } from 'aws-amplify';
-function App() {
-  const [user, updateUser] = React.useState(null);
-  React.useEffect(() => {
-    Auth.currentAuthenticatedUser()
-      .then(user => updateUser(user))
-      .catch(() => console.log('No signed in user.'));
-    Hub.listen('auth', data => {
-      switch (data.payload.event) {
-        case 'signIn':
-          return updateUser(data.payload.data);
-        case 'signOut':
-          return updateUser(null);
-      }
-    });
-  }, [])
-  if (user) {
-    return (
-      <div>
-        <h1>Hello {user.username}</h1>
-        <AmplifySignOut />
-      </div>
-    )
-  }
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <AmplifyAuthenticator>
-        <AmplifySignUp
-          slot="sign-up"
-          formFields={[
-            { type: "username" },
-            {
-              type: "password",
-              label: "Custom Password Label",
-              placeholder: "custom password placeholder"
-            },
-            { type: "email" }
-          ]} 
-        />
-      </AmplifyAuthenticator>
-    </div>
-  );
-}
-export default App
-
 //#802 page color 
-// class LoginPage extends React.Component {
-//     render() {
-//         return (
-//             <div id="login-page">
-//                 <div className="signin">
-//                     <h1> Sign In </h1>
-//                     <div id="user-form">
-//                         <input name= "E-Mail" placeholder="E-Mail"/>
-//                         <input name= "Password" placeholder="Password"/>
-//                         <button id="forgotbttn"> Forgot Password? </button>
-//                         <button> Sign In </button>
-//                     </div>
-//                 </div>
-//                 <div className="signUp">
-//                     <h1> Sign Up </h1>
-//                     <div id="new-user-signup">
-//                         <input name="E-Mail" placeholder="E-Mail"/>
-//                         <input name="Password" placeholder="Password"/>
-//                         <input name="Confirm Password" placeholder="Confirm Password"/>
-//                         <button> Sign Me Up </button>
-//                     </div>
-//                 </div>
-//                 <div className="forgot-password">
+class LoginPage extends React.Component {
+    render() {
+        return (
+            <div id="login-page">
+                <div className="signin">
+                    <h1> Sign In </h1>
+                    <div id="user-form">
+                        <input name= "E-Mail" placeholder="E-Mail"/>
+                        <input name= "Password" placeholder="Password"/>
+                        <button id="forgotbttn"> Forgot Password? </button>
+                        <button> Sign In </button>
+                    </div>
+                </div>
+                <div className="signUp">
+                    <h1> Sign Up </h1>
+                    <div id="new-user-signup">
+                        <input name="E-Mail" placeholder="E-Mail"/>
+                        <input name="Password" placeholder="Password"/>
+                        <input name="Confirm Password" placeholder="Confirm Password"/>
+                        <button> Sign Me Up </button>
+                    </div>
+                </div>
+                <div className="forgot-password">
 
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
+                </div>
+            </div>
+        )
+    }
+}
 
-// export default LoginPage;
+export default LoginPage;
