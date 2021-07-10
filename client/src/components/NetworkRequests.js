63291021fbd4564edc9da3dd9a53cca81cfb21e7
Abe Jay <officialjabe@gmail.com>
2021-07-10 03:02:37 -0400

    heroku deploy because life

--- client/src/components/NetworkRequests.js
+++ client/src/components/NetworkRequests.js
@@ -1,6 +1,7 @@
 import { getTokenFromStorage } from './utils';
+const endPoint = `https://ofg-raffle.herokuapp.com/api`;
 // const endPoint = `http://localhost:3030/api`;
-const endPoint = `https://main.d39h9sudxy5itw.amplifyapp.com/api`;
+// const endPoint = `https://main.d39h9sudxy5itw.amplifyapp.com/api`;
 
 
 export const getTable = async (table) => {
